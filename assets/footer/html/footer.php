6a07d77afe855cd4ecdc80d8884a8fa025d9d278
michael.mg <michael.mg.09a@gmail.com>
2024-11-21 13:57:13 +0100

    add footer, slider, images

---
+++ assets/footer/html/footer.php
@@ -0,0 +1,54 @@
+<footer class="footer">
+    <fieldset class="footer__fieldset_resume">
+        <legend>Résumé de Arcadia - ZooParc</legend>
+        <p>Le parc animalier offre une expérience immersive avec une grande variété d'animaux exotiques.
+            Des visites guidées, des spectacles animaliers et des aires de jeux pour enfants sont disponibles.
+            Vous trouverez aussi des restaurants, des boutiques de souvenirs et des espaces de détente pour une journée inoubliable en famille.
+        </p>
+        <footer class="container_icons_payments">
+            <p>Moyens de paiements</p>
+            <span>
+                <img src="/assets/footer/images_&_logos/payment_mastercard.webp" alt="Icon mastercard" title="Mastercard">
+                <img src="/assets/footer/images_&_logos/payment_maestro.webp" alt="Icon maestro" title="Maestro">
+                <img src="/assets/footer/images_&_logos/payment_paypal.webp" alt="Icon Paypal" title="Paypal">
+                <img src="/assets/footer/images_&_logos/payment_visa.webp" alt="Icon visa" title="Visa">
+            </span>
+        </footer>
+    </fieldset>
+    <fieldset class="footer__fieldset_info_park">
+        <legend>Park information</legend>
+        <section>
+            <span>Address</span>
+            <span>10 zoopark street</span>
+            <span>09100 the forest</span>
+            <span><br>Tel : 07 01 02 03 04</span>
+        </section>
+    </fieldset>
+    <fieldset class="footer__fieldset_opening">
+        <legend>Opening hours</legend>
+        <ul class="footer__opening_ul">
+            <li class="footer__opening_ul__li">Monday : 9h - 17h30</li>
+            <li class="footer__opening_ul__li">Thuesday : 9h - 17h30</li>
+            <li class="footer__opening_ul__li">Wednesday : 9h - 18h30</li>
+            <li class="footer__opening_ul__li">Thursday : 9h - 17h30</li>
+            <li class="footer__opening_ul__li">Friday : 9h - 17h30</li>
+            <li class="footer__opening_ul__li">Saturday : 9h - 19h30</li>
+            <li class="footer__opening_ul__li">Sunday : 9h - 19h30</li>
+        </ul>
+    </fieldset>
+    <fieldset class="footer__fieldset_socials">
+        <legend>Social network links</legend>
+        <img src="/assets/footer/images_&_logos/x_twitter.webp" alt="Icon twitter" title="Twitter">
+        <img src="/assets/footer/images_&_logos/facebook.webp" alt="Icon facabook" title="Facebook">
+        <img src="/assets/footer/images_&_logos/instagram.webp" alt="Icon instagram" title="Instagram">
+    </fieldset>
+    <fieldset class="footer__fieldset_internal">
+        <legend>Internal Links</legend>
+        <ul class="footer__links_ul">
+            <li class="footer__links_ul__li"><a class="footer__links_ul__li__a" href="#">Politique de confidentialité</a></li>
+            <li class="footer__links_ul__li"><a class="footer__links_ul__li__a" href="#">Mentions légales</a></li>
+            <li class="footer__links_ul__li"><a class="footer__links_ul__li__a" href="#">Nous contacter</a></li>
+        </ul>
+    </fieldset>
+    <p class="copyright">© 2024 - Arcadia ZooParc<br>- All rights reserved -</p>
+</footer>
